2ff21a1565e78aeb38759407d09a74428f5138f8
Ebaahinaii <eaah1d22@soton.ac.uk>
2026-02-03 11:47:21 +0000

    Solar system and Build buttons navigates to new windows

---
+++ src/main/java/uk/ac/soton/comp2300/scene/SolarSystemScene.java
@@ -0,0 +1,39 @@
+package uk.ac.soton.comp2300.scene;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import uk.ac.soton.comp2300.ui.MainPane;
+import uk.ac.soton.comp2300.ui.MainWindow;
+
+public class SolarSystemScene extends BaseScene {
+    public SolarSystemScene(MainWindow mainWindow) { super(mainWindow); }
+
+    @Override
+    public void build() {
+        root = new MainPane(mainWindow.getWidth(), mainWindow.getHeight());
+        root.setStyle("-fx-background-color: black;");
+
+        // Back button to return to the planet
+        Button btnBack = new Button("⬅");
+        btnBack.setPrefSize(50, 50);
+        btnBack.getStyleClass().add("menu-icon-button");
+        btnBack.setOnAction(e -> mainWindow.loadScene(new MenuScene(mainWindow)));
+
+        StackPane.setAlignment(btnBack, Pos.TOP_LEFT);
+        StackPane.setMargin(btnBack, new Insets(20));
+
+        VBox container = new VBox();
+        container.setAlignment(Pos.CENTER);
+        Label title = new Label("Solar System View");
+        title.setStyle("-fx-text-fill: white; -fx-font-size: 32px; -fx-font-weight: bold;");
+
+        container.getChildren().add(title);
+        root.getChildren().addAll(container, btnBack);
+    }
+
+    @Override public void initialise() {}
+}
