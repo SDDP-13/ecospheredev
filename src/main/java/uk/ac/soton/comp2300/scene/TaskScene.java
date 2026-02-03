e1aff93ea5bd27ecc38e91c770c8c69154a131e3
Ebaahinaii <eaah1d22@soton.ac.uk>
2026-02-03 11:18:07 +0000

    All drop down menu buttons navigate to a new window

---
+++ src/main/java/uk/ac/soton/comp2300/scene/TaskScene.java
@@ -0,0 +1,37 @@
+package uk.ac.soton.comp2300.scene;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import uk.ac.soton.comp2300.ui.MainPane;
+import uk.ac.soton.comp2300.ui.MainWindow;
+
+public class TaskScene extends BaseScene {
+    public TaskScene(MainWindow mainWindow) { super(mainWindow); }
+
+    @Override
+    public void build() {
+        root = new MainPane(mainWindow.getWidth(), mainWindow.getHeight());
+        root.setStyle("-fx-background-color: black;");
+
+        Button btnBack = new Button("⬅");
+        btnBack.setPrefSize(50, 50);
+        btnBack.getStyleClass().add("menu-icon-button");
+        btnBack.setOnAction(e -> mainWindow.loadScene(new MenuScene(mainWindow)));
+        StackPane.setAlignment(btnBack, Pos.TOP_LEFT);
+        StackPane.setMargin(btnBack, new Insets(20));
+
+        VBox container = new VBox();
+        container.setAlignment(Pos.CENTER);
+        Label title = new Label("Tasks Window");
+        title.setStyle("-fx-text-fill: white; -fx-font-size: 32px; -fx-font-weight: bold;");
+
+        container.getChildren().add(title);
+        root.getChildren().addAll(container, btnBack);
+    }
+
+    @Override public void initialise() {}
+}
