872ebb18f8021da24d8aea4b832c7ea225a92fb6
Yuma K <yr2g23@soton.ac.uk>
2026-04-23 10:59:38 +0100

    Help page update

--- src/main/java/uk/ac/soton/comp2300/recommendation_logic/RecommendationService.java
+++ src/main/java/uk/ac/soton/comp2300/recommendation_logic/RecommendationService.java
@@ -11,9 +11,9 @@ public class RecommendationService {
     private CarbonIntensityService carbonService = new CarbonIntensityService();
 
     // Most recently fetched forecast slots from the API
-    private List<CarbonSlot> cachedForecastSlots = null;
+    private static List<CarbonSlot> cachedForecastSlots = null;
     // When it was fetched
-    private LocalDateTime cacheTimestamp = null;
+    private static LocalDateTime cacheTimestamp = null;
     private static final int CACHE_EXPIRY_HOURS = 2;
 
     public String getRecommendation(String appliance) {
@@ -64,7 +64,7 @@ public class RecommendationService {
             + " (carbon intensity: " + bestSlot.getForecast() + " gC02/kWh)";
                 
         if (outputDataSource.startsWith("cached")) {
-            recommendation += "Based on " + outputDataSource + " data - live feed unavailable.";
+            recommendation += " Based on " + outputDataSource + " data - live feed unavailable.";
         }
 
         return recommendation;
--- src/main/java/uk/ac/soton/comp2300/scene/HelpScene.java
+++ src/main/java/uk/ac/soton/comp2300/scene/HelpScene.java
@@ -9,8 +9,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
 import uk.ac.soton.comp2300.ui.MainPane;
 import uk.ac.soton.comp2300.ui.MainWindow;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class HelpScene extends BaseScene {
     public HelpScene(MainWindow mainWindow) { super(mainWindow); }
@@ -37,24 +40,42 @@ public class HelpScene extends BaseScene {
 
         Label heading = new Label("How to use Ecosphere");
         heading.getStyleClass().add("title-large");
+        heading.setStyle("-fx-font-weight: bold;");
 
-        Label section1 = new Label("1. Login");
-        section1.setStyle("-fx-font-weight: bold");
-        Label section1Text = new Label("If you are having trouble logging in, ....");
 
-        Label section2 = new Label("2. Scheduling");
-        section2.setStyle("-fx-font-weight: bold");
-        Label section2Text = new Label("To schedule an appliance, ....");
 
-        Label faqLabel = new Label("FAQ:");
-        faqLabel.setStyle("-fx-font-weight: bold");
-        
-        Image help_schedule = new Image(getClass().getResource("/images/EcosphereHelp_AddSchedule.png").toExternalForm());
-        ImageView imageView = new ImageView(help_schedule);
-        imageView.setFitWidth(300);
-        imageView.setPreserveRatio(true);
+        Label login = new Label("1. Login");
+        login.setStyle("-fx-font-weight: bold;");
+        Label section1Text = new Label("The Login Page allows users to securely access their account or create a new one.");
+        section1Text.setWrapText(true);
+        Image help_login1 = new Image(getClass().getResource("/images/Help_login1.png").toExternalForm());
+        ImageView login1_view = new ImageView(help_login1);
+        login1_view.setFitWidth(300);
+        login1_view.setPreserveRatio(true);
 
-        helpContent.getChildren().addAll(heading, section1, section2, faqLabel, imageView);
+        TextFlow loginTextFlow = new TextFlow();
+        Text login1 = new Text(" • Enter your username and password to log in\n");
+        Text login2 = new Text(" • ");
+        Text login3 = new Text("New users");
+        login3.setStyle("-fx-font-weight: bold;");
+        Text login4 = new Text(" can select 'Create Account' to register\n");
+        Text login5 = new Text(" • Usernames must be unique");
+        loginTextFlow.getChildren().addAll(login1, login2, login3, login4, login5);
+        Image help_login2 = new Image(getClass().getResource("/images/Help_login2.png").toExternalForm());
+        ImageView login2_view = new ImageView(help_login2);
+        login2_view.setFitWidth(300);
+        login2_view.setPreserveRatio(true);
+
+
+
+        helpContent.getChildren().addAll(
+            heading,
+            login,
+            section1Text,
+            login1_view,
+            loginTextFlow,
+            login2_view
+        );
 
         scrollPane.setContent(helpContent);
 
